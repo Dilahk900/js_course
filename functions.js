97aaae68d26bde225fccfb0f1a131cc8175ab003
dilahk <khalid.idrissif@gmail.com>
2025-05-17 01:58:22 +0200

    code improved 17/05

--- functions.js
+++ functions.js
@@ -1,46 +1,64 @@
-function _cp_choice(){
-          const $choice = Math.random(); //azeaze
+          /*let $player_score = 0;
+          let $cp_score = 0;*/
+          const $score = {
+            wins : 0,
+            losses : 0,
+            ties : 0
+          }
+          
+          function cp_choice(){
+          const $choice = Math.random();
           let $result;
           let $pc_chose = '';
+
           if ($choice <= 1 / 3){ 
-             $pc_chose = "rock";       
+             $pc_chose = 'rock';       
           }
           else if($choice > 1 / 3 && $choice <= 2 / 3){
-             $pc_chose = "paper";
+             $pc_chose = 'paper';
           } 
           else{
-            $pc_chose = "scissors";
+            $pc_chose = 'scissors';
           }
           
           return($pc_chose);
       }
 
-      function _game(player_choice){
-          
-          let $pc_choice = _cp_choice();
+      function game(player_choice){
+
+          let $pc_choice = cp_choice();
+
           if(player_choice === 'rock'){
             if($pc_choice === 'scissors' ){
-              $result = "you win !";
+              $result = 'you win !';
             } else if($pc_choice === 'paper'){
-              $result = "you lose !";
-            } else  $result = "Tie !";
+              $result = 'you lose !';
+            } else  $result = 'Tie !';
           }
 
           if(player_choice === 'paper'){
             if($pc_choice === 'scissors' ){
-              $result = "you lose !";
+              $result = 'you lose !';
             } else if($pc_choice === 'rock'){
-              $result = "you win !";
-            } else  $result = "Tie !";
+              $result = 'you win !';
+            } else  $result = 'Tie !';
           }
 
           if(player_choice === 'scissors'){
             if($pc_choice === 'rock' ){
-              $result = "you lose !";
+              $result = 'you lose !';
             } else if($pc_choice === 'paper'){
-              $result = "you win !";
-            } else  $result = "Tie !";
+              $result = 'you win !';
+            } else  $result = 'Tie !';
           }
+
+          if($result === 'you lose !'){
+         /*   $cp_score += 1;*/
+              $score.losses += 1;
+          } else if($result === 'you win !'){
+         /*   $player_score += 1;*/
+              $score.wins += 1;
+          } else $score.ties += 1;
 
 
           /*if(player_choice === 'rock' && $pc_choice === 'scissors'){
@@ -64,6 +82,6 @@ function _cp_choice(){
           console.log("Computer chose : " + $pc_choice + " and you chose " + player_choice);
           console.log("Result : " + $result);
           document.getElementById("pc_choice").textContent = $pc_choice;
-          document.getElementById("player_choice").textContent = player_choice;
+          document.getElementById("player_choice").textContent = player_choice + '(WINS : ' + $score.wins +') (LOSSES ' + $score.losses +') (TIES ' + $score.ties +')';
           document.getElementById("result").textContent = $result;
       }
