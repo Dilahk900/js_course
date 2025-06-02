89774be76a15a0be6248e9077b582792d8b8c326
Dilahk900 <khalid.idrissif@gmail.com>
2025-06-03 00:09:29 +0200

    array courses

--- Array/todo-list.js
+++ Array/todo-list.js
@@ -4,6 +4,9 @@ let list = [];
 function add_task(){
 
     list.push(document.querySelector('.new-task').value);
-    console.log(list);
+    for(let i = 0; i < list.length; i++){
+     console.log(list[i]);        
+    }
+    document.querySelector('.new-task').value = ``;
 }
 
