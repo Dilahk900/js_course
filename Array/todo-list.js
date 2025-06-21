4656e8d08c8dad33a9553a3062b7e5c946d805a4
Dilahk900 <khalid.idrissif@gmail.com>
2025-06-22 01:55:55 +0200

    todo list finished !

--- Array/todo-list.js
+++ Array/todo-list.js
@@ -1,12 +1,28 @@
+let todoList = [];
+let newTaskinputElement = document.querySelector('.js-new-task');
+let newDateinputElement = document.querySelector('.js-date');
+let todoListDisplay = document.querySelector('.js-tasks');
 
-let list = [];
 
-function add_task(){
-
-    list.push(document.querySelector('.new-task').value);
-    for(let i = 0; i < list.length; i++){
-     console.log(list[i]);        
-    }
-    document.querySelector('.new-task').value = ``;
+function add_todo(){
+  let newTask = newTaskinputElement.value;
+  let newDate = newDateinputElement.value;
+  todoList.push({newTask, newDate});
+  newTaskinputElement.value = '';
+  renderTodolist();
 }
 
+function renderTodolist(){
+  let html = '';
+  for(let i = 0 ; i < todoList.length ; i++){
+    const todo = todoList[i].newTask;
+    const todoD = todoList[i].newDate;
+    html += `<div>${todo}</div>
+        <div> ${todoD }</div> 
+         <button onclick="
+         todoList.splice(${i} , 1);
+         renderTodolist();
+         " class = "dlt-btn">DELETE</button>`;
+  }
+  todoListDisplay.innerHTML = html;
+}
